8ae3c80f712cdaf86f7e4bf6aa40f738392b63e1
arslansaeed <arslansaeedq@yahoo.com>
2026-02-25 13:06:11 +0000

    Time: 3 ms (5.95%) | Memory: 19.5 MB (21.22%) - LeetSync

---
+++ 198-house-robber/house-robber.py
@@ -0,0 +1,59 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n  = len(nums)
+        memo = {}
+
+        def dfs(idx: int):
+            if idx >=  n:
+                return 0
+
+            # if memo.get(idx, 0):
+            #     return memo[idx]
+
+            if idx in memo:
+                return memo[idx]
+
+            robbed = nums[idx] + dfs(idx +2)
+            skipped  = dfs(idx+1)
+
+            max_robb = max(robbed, skipped)
+            memo[idx] = max_robb
+
+            return max_robb   
+
+        return dfs(0)
+
+
+
+
+    # def rob(self, nums: List[int]) -> int:
+    #     n  = len(nums)
+    #     output  = 0
+
+    #     def dfs(idx: int, amount: int):
+    #         nonlocal output
+
+    #         #print(f"idx: {idx}")
+    #         if idx > n-1:
+    #             print(f"amount: {amount}, output:{output}")
+    #             output = max(output, amount)
+    #             return
+
+    #         amount += nums[idx]
+    #         #print(f"amount: {amount}")
+    #         for i in range(idx+2, n+2):                
+    #             dfs(i, amount)
+
+        
+    #     dfs(0,0)
+    #     dfs(1,0)
+
+    #     return output
+            
+
+        
+
+            
+           
+
+        
