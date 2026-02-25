5bdaad66f396834cf1d0e4358984747d899c22b9
arslansaeed <arslansaeedq@yahoo.com>
2026-02-25 14:00:54 +0000

    Time: 38 ms (87.80%) | Memory: 19.3 MB (16.54%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,11 +1,17 @@
+# fab(n) is n+1 element in fab serious if starting from 0 index
+# fibonacci serious starting from 0 -   0, 1,1,2,3,5,8    if we start from 0 then for n=6  we need to go n+1 = 7 
+# finoacci serious starting from 1         1,1,2,3,5,8    if we start from 1 then for n= 6 we need to go to n = 6
+
 class Solution:
+    # solved using recurssion
     def fib_recur(self, n: int) -> int:
         if n <= 1:
             return n
 
         return self.fib(n-1) + self.fib(n-2)
         
-    def fib(self, n: int) -> int:
+    # solved using sliding window
+    def fib_sliding (self, n: int) -> int:
         if n <= 1:
             return n
 
@@ -15,4 +21,16 @@ class Solution:
 
         return curr 
 
-        
+    # solved using tabulation - bottom up dp
+    def fib(self, n: int) -> int:
+        if n <= 1:
+            return n
+
+        dp = [0] * (n+1)
+        dp[0] = 0
+        dp[1] = 1
+
+        for i in range(2, n+1):
+            dp[i] = dp [i-1] + dp[i -2]
+
+        return dp[n]
