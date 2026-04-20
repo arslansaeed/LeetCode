d7ecbed1a0e2f990f285deb334818c7ea7bfc771
arslansaeed <arslansaeedq@yahoo.com>
2026-04-20 12:21:47 +0100

    Time: 0 ms (100.00%) | Memory: 19.6 MB (17.72%) - LeetSync

--- 1603-running-sum-of-1d-array/running-sum-of-1d-array.py
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.py
@@ -1,10 +1,18 @@
 class Solution:
-    def runningSum(self, nums: List[int]) -> List[int]:
+    def runningSum_iterative(self, nums: List[int]) -> List[int]:
 
         for i in range (1, len(nums)):
             nums[i] += nums[i-1]
 
         return nums
 
+    def runningSum(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        def recursion(idx = 1):
+            if idx == n:
+                return 
+            nums[idx] += nums[idx -1]                       
+            recursion(idx + 1)
 
-        
+        recursion()
+        return nums
