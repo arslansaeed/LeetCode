a6db6ed4db4f2ca9180e62e02a86c531b4dde93a
arslansaeed <arslansaeedq@yahoo.com>
2026-05-01 08:49:25 +0100

    Time: 3 ms (39.33%) | Memory: 24.4 MB (5.13%) - LeetSync

--- 113-path-sum-ii/path-sum-ii.py
+++ 113-path-sum-ii/path-sum-ii.py
@@ -13,9 +13,7 @@ class Solution:
             if not node:                
                 return 
             
-
-            path.append(node.val)
-           
+            path.append(node.val)           
 
             local_sum  += node.val
             dfs(node.left, local_sum,path[:])
@@ -27,10 +25,30 @@ class Solution:
             else:
                 dfs(node.right, local_sum,path[:])
 
-            return local_sum
+            #return local_sum
         
         dfs(root, 0,[])
  
         return output
 
+
+# class Solution:
+#     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+#         self.result = []
+#         def dfs(node: Optional[TreeNode], local_sum, list1):
+#             list2 = list1[:]
+#             if  node == None:
+#                 return
+
+#             local_sum  += node.val
+#             list2.append(node.val)
+#             if not node.left and not node.right:
+#                 if local_sum == targetSum:
+#                     self.result.append(list2)
+#                     return
+#             dfs(node.left, local_sum,list2)
+#             dfs(node.right, local_sum,list2)
+
+#         dfs (root, 0, [])
+#         return self.result
     
