79c050df5dff15992708215493cb82b1f5f95d93
arslansaeed <arslansaeedq@yahoo.com>
2026-05-01 18:35:43 +0100

    Time: 1 ms (49.03%) | Memory: 20.1 MB (64.49%) - LeetSync

--- 113-path-sum-ii/path-sum-ii.py
+++ 113-path-sum-ii/path-sum-ii.py
@@ -11,24 +11,24 @@ class Solution:
         def dfs(node: Optional[TreeNode], local_sum, path):
             if not node:                
                 return 
-            path1 = path[:] 
-            path1.append(node.val)
+            #path1 = path[:] 
+            path.append(node.val)
             local_sum  += node.val
 
           
             if (not node.left) and (not node.right):
                 if local_sum == targetSum:
-                    output.append(path1)
+                    output.append(path[:])
                     #output.append(path)
                     # we can remove below 2 line and let it run line 26,27 and do pop as
                     # return at line 12-13 will run quickly that solution is also fine
                     # path.pop()
                     # return
                     
-            dfs(node.left, local_sum,path1)            
-            dfs(node.right, local_sum,path1)
+            dfs(node.left, local_sum,path)            
+            dfs(node.right, local_sum,path)
 
-            #path.pop()
+            path.pop()
 
             #return local_sum
         
