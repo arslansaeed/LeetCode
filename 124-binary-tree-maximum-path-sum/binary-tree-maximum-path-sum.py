e62769b8c5c9af72906d70600b3f24364ad24d14
arslansaeed <arslansaeedq@yahoo.com>
2026-05-14 17:39:45 +0100

    Time: 15 ms (15.21%) | Memory: 23.9 MB (31.14%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.py
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.py
@@ -6,26 +6,63 @@
 #         self.right = right
 class Solution:
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
-        if not root.left and not root.right:
-            return root.val
-
-        self.pathsum = float("-inf")
-
-        def dfs(node):
-            if not node:
+        self.max_sum  = float('-inf')
+         
+        def dfs(root):
+            if not root:
                 return 0
             
-            left_sum = max(dfs(node.left), 0)
-            right_sum = max(dfs(node.right) ,0)
+            l_val  = dfs(root.left)
+            r_val = dfs(root.right)
 
-            t_sum = left_sum + right_sum + node.val
-            if t_sum > self.pathsum:
-                self.pathsum = t_sum 
-            # elif node.val > self.pathsum:
-            #     self.pathsum  = node.val
-            # max_child = max(left_sum, right_sum)
-            # return max(max_child + node.val , node.val)
-            return max(left_sum, right_sum) + node.val
+            c_l_val = root.val + l_val
+            c_r_val = root.val + r_val
+
+            self.max_sum = max(self.max_sum, root.val, root.val + l_val + r_val, root.val + l_val,  root.val + r_val  )
+            return max( root.val + l_val, root.val + r_val, root.val )
 
         dfs(root)
-        return self.pathsum
+        return self.max_sum
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # def maxPathSum(self, root: Optional[TreeNode]) -> int:
+    #     if not root.left and not root.right:
+    #         return root.val
+
+    #     self.pathsum = float("-inf")
+
+    #     def dfs(node):
+    #         if not node:
+    #             return 0
+            
+    #         left_sum = max(dfs(node.left), 0)
+    #         right_sum = max(dfs(node.right) ,0)
+
+    #         t_sum = left_sum + right_sum + node.val
+    #         if t_sum > self.pathsum:
+    #             self.pathsum = t_sum 
+
+    #         return max(left_sum, right_sum) + node.val
+
+    #     dfs(root)
+    #     return self.pathsum
