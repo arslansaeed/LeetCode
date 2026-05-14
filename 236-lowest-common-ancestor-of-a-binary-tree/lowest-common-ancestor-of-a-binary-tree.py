cb7605b021b94f19a63ca00a2fa3237fe66cab73
arslansaeed <arslansaeedq@yahoo.com>
2026-05-14 15:47:34 +0100

    Time: 67 ms (21.07%) | Memory: 24.5 MB (54.74%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
@@ -5,40 +5,29 @@
 #         self.left = None
 #         self.right = None
 
-# class Solution:
-#     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-#         if not root or root == p or root == q:
-#             return root
-
-#         left = self.lowestCommonAncestor(root.left, p, q)
-#         right = self.lowestCommonAncestor(root.right, p, q)
-
-#         # if l_node and not r_node:
-#         #     return l_node
-
-#         # if r_node and not l_node:
-#         #     return r_node
-        
-#         # if l_node and r_node:
-#         #     return root
-
-#         if left and right:
-#             return root
-        
-#         return left if left else right
-
 class Solution:
-    def lowestCommonAncestor(self, root, p, q):
-        if not root or root == p or root == q:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+
+        if not root:
+            return 
+
+        if root == p or root == q:
             return root
-        
+
         left = self.lowestCommonAncestor(root.left, p, q)
         right = self.lowestCommonAncestor(root.right, p, q)
-        
-        if left and right:
+
+        if not left and not right:
+            return 
+
+        if left and  right:
             return root
-        
-        return left if left else right
+
+        if left:
+            return left
+
+        if right:
+            return right
 
 
 
