8ac9ef32fbc44dab5faf05f49b41023182dfd113
arslansaeed <arslansaeedq@yahoo.com>
2026-05-18 18:52:04 +0100

    Time: 137 ms (23.98%) | Memory: 31.9 MB (68.98%) - LeetSync

--- 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
@@ -4,28 +4,43 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def goodNodes(self, root: TreeNode) -> int:
-        #self.count = 0
+# class Solution:
+#     def goodNodes(self, root: TreeNode) -> int:
+#         #self.count = 0
 
-        def dfs(node, cur_max):
-            # base confition
-            if not node:
-                return 0
+#         def dfs(node, cur_max):
+#             # base confition
+#             if not node:
+#                 return 0
 
             
-            if cur_max <= node.val:
-                cur_max =  node.val
-                count = 1
-            else:
-                count  = 0
+#             if cur_max <= node.val:
+#                 cur_max =  node.val
+#                 count = 1
+#             else:
+#                 count  = 0
 
 
-            count += dfs(node.left, cur_max)
-            count += dfs(node.right, cur_max)
+#             count += dfs(node.left, cur_max)
+#             count += dfs(node.right, cur_max)
+
+#             return count
+
+#         return dfs(root, root.val)
+
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        
+        def dfs(root,maxVal):
+            if not root:
+                return 0
+            
+            count = 1 if root.val >= maxVal else 0
+            maxVal = max(maxVal,root.val)
+
+            count+=dfs(root.left,maxVal)
+            count+=dfs(root.right,maxVal)
 
             return count
-
-        return dfs(root, root.val)
-
+        return dfs(root,root.val)
         
