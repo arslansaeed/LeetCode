106bc9beb649aa4aa1d8b8dcc4bc50e331817d62
arslansaeed <arslansaeedq@yahoo.com>
2026-05-19 20:15:33 +0100

    Time: 7 ms (61.15%) | Memory: 20.3 MB (56.65%) - LeetSync

--- 437-path-sum-iii/path-sum-iii.py
+++ 437-path-sum-iii/path-sum-iii.py
@@ -6,6 +6,68 @@
 #         self.right = right
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
+        dic = {0 : 1}
+
+        def dfs(node, cur_sum):
+            #base condition
+            if not node:
+                return 0
+
+            cur_sum  += node.val
+            if (cur_sum - targetSum) in dic and  dic[cur_sum - targetSum] > 0:
+                count = dic[cur_sum - targetSum]
+            else:
+                count =0
+
+            if cur_sum in dic:
+                dic[cur_sum] += 1
+            else:
+                dic[cur_sum] = 1
+
+            count += dfs(node.left, cur_sum)
+            count += dfs(node.right, cur_sum)
+
+            dic[cur_sum] -= 1
+
+            return count
+
+        return dfs(root, 0)
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    def pathSum_ours(self, root: Optional[TreeNode], targetSum: int) -> int:
         self.counter  = 0
 
         def dfs(node, path):
