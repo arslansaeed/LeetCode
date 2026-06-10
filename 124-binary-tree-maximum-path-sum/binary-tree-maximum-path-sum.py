6f41538e17978cc2a83fb0888556bb8d76a831ea
arslansaeed <arslansaeedq@yahoo.com>
2026-06-10 19:20:26 +0100

    Time: 8 ms (60.33%) | Memory: 23.9 MB (62.13%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.py
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.py
@@ -6,19 +6,51 @@
 #         self.right = right
 class Solution:
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        self.max_path_val = float("-inf")
 
-        self.max_path_sum = float("-inf")
         def dfs(node):
             #base condition
             if not node:
                 return 0
 
-            left_sum = dfs(node.left)
-            right_sum = dfs(node.right)
+            left_val = dfs(node.left)
+            right_val = dfs(node.right)
 
-            self.max_path_sum = max(self.max_path_sum, node.val, left_sum + node.val, right_sum+node.val, node.val + right_sum+ left_sum )            
-            return max(node.val, left_sum + node.val, right_sum+node.val )
+            # max path sum till that node
+            node_max_val = max((node.val + left_val + right_val), node.val, node.val+ left_val, node.val + right_val)
+            self.max_path_val = max(self.max_path_val,node_max_val )
+
+            sub_path_val = max( node.val, node.val+ left_val, node.val + right_val)
+            return sub_path_val
 
         dfs(root)
-        return self.max_path_sum
+        return  self.max_path_val
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # self.max_path_sum = float("-inf")
+        # def dfs(node):
+        #     #base condition
+        #     if not node:
+        #         return 0
+
+        #     left_sum = dfs(node.left)
+        #     right_sum = dfs(node.right)
+
+        #     self.max_path_sum = max(self.max_path_sum, node.val, left_sum + node.val, right_sum+node.val, node.val + right_sum+ left_sum )            
+        #     return max(node.val, left_sum + node.val, right_sum+node.val )
+
+        # dfs(root)
+        # return self.max_path_sum
         
