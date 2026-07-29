d4bb073ff68df9efb341ee855194919212281b1e
arslansaeed <arslansaeedq@yahoo.com>
2026-07-29 19:22:20 +0100

    Time: 0 ms (100.00%) | Memory: 19.4 MB (72.19%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -5,14 +5,17 @@ class Solution:
         def dfs(curr, opened,closed):
             # base condition
             if len(curr) == 2*n:
-                output.append(curr)               
+                output.append(curr) 
+                #print(f"in base condition curr is {curr}")              
                 return
 
             if opened < n:       
                 dfs(curr + "(", opened +1, closed)
 
+            #print(f"after opened dfs curr is {curr}") 
             if closed < opened:
                 dfs(curr + ")", opened , closed +1)
+            #print(f"after closed dfs curr is {curr}") 
 
 
         dfs("", 0, 0 )
