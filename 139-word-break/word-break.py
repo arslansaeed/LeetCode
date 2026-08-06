3f4c23393a99ec795084668fc3bcde399155e5cb
arslansaeed <arslansaeedq@yahoo.com>
2026-08-06 19:06:52 +0100

    Time: 12 ms (5.05%) | Memory: 19.6 MB (9.45%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -8,16 +8,19 @@ class Solution:
                 return True
 
             if idx in db:
+                print(f"db in base condition : {db}")
                 return db[idx]
 
 
             for word in wordDict:
                 if s.startswith(word, idx):                      
-                    if dfs(idx+ len(word)):
-                        db[idx] = True
+                    if dfs(idx + len(word)):
+                        #db[idx] = True
+                        #print(f"db when match is successful : {db}")
                         return True
 
             db[idx] = False
+            print(f"db when match is not successful : {db}")
             return False                    
 
         return dfs(0)
